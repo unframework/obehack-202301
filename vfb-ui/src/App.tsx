0558a91c69da6dfb2d4aaf2bdd236ff76f50c5ed
Nick Matantsev <nick.matantsev@gmail.com>
2023-02-17 01:58:45 -0500

    Try to load generated Emscripten module as dynamic import

--- vfb-ui/src/App.tsx
+++ vfb-ui/src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { VFBCanvas } from './VFBCanvas';
+
+const CODE_SERVER_URL = 'http://localhost:3000';
 
 const dummyBuffer = new Uint8Array(256);
 dummyBuffer[2] = 123;
 
 export const App = () => {
+  useEffect(() => {
+    import(/* webpackIgnore: true */ `${CODE_SERVER_URL}/vfb_main.js`).then(
+      (vfbModule) => {
+        console.log('imported', vfbModule);
+      }
+    );
+  }, []);
+
   return (
     <div className="flex flex-column justify-center items-center w-full h-full">
       <div className="flex border border-neutral-600">
