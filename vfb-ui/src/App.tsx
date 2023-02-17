3fd3721a5f04b1a321d913fc4830b66b10aba39e
Nick Matantsev <nick.matantsev@gmail.com>
2023-02-16 22:46:43 -0500

    Implement buffer rendering to canvas

--- vfb-ui/src/App.tsx
+++ vfb-ui/src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 import { VFBCanvas } from './VFBCanvas';
 
+const dummyBuffer = new Uint8Array(256);
+dummyBuffer[2] = 123;
+
 export const App = () => {
   return (
     <div className="flex flex-column justify-center items-center w-full h-full">
       <div className="flex border border-neutral-600">
-        <VFBCanvas />
+        <VFBCanvas buffer={dummyBuffer} />
       </div>
     </div>
   );
--- vfb-ui/src/VFBCanvas.tsx
+++ vfb-ui/src/VFBCanvas.tsx
@@ -1,10 +1,52 @@
 import React, { useEffect, useRef } from 'react';
 
-export const VFBCanvas = () => {
+const WIDTH = 360;
+const HEIGHT = 520;
+
+const MARGIN_X = 20;
+const MARGIN_Y = 30;
+const PIX_W = 10;
+const PIX_H = 10;
+
+const PIX_STRIDE_X = (WIDTH - MARGIN_X * 2 - PIX_W) / 15;
+const PIX_STRIDE_Y = (HEIGHT - MARGIN_Y * 2 - PIX_H) / 15;
+
+const fills = [
+  '#000000',
+  '#1f1f1f', // starts a bit brighter than 1/16th
+  '#2f2f2f',
+  '#3f3f3f',
+  '#4f4f4f',
+  '#5f5f5f',
+  '#6f6f6f',
+  '#7f7f7f',
+  '#8f8f8f',
+  '#9f9f9f',
+  '#afafaf',
+  '#bfbfbf',
+  '#cfcfcf',
+  '#dfdfdf',
+  '#efefef',
+  '#ffffff',
+];
+
+export const VFBCanvas: React.FC<{ buffer: Uint8Array }> = ({ buffer }) => {
+  if (buffer.length !== 256) {
+    throw new Error('expecting 256 byte render buffer');
+  }
+
+  // stash inside ref to avoid resetting effect
+  const bufferRef = useRef(buffer);
+  bufferRef.current = buffer;
+
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
     const ctx = canvasRef.current!.getContext('2d')!;
+
+    // opaque background
+    ctx.fillStyle = '#000';
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
     let currentCallbackId: number = -1;
 
@@ -12,9 +54,25 @@ export const VFBCanvas = () => {
       // schedule next
       currentCallbackId = requestAnimationFrame(rafCallback);
 
-      ctx.fillStyle = '#888';
-      ctx.rect(10, 10, 10, 10);
-      ctx.fill();
+      ctx.fillStyle = 'rgba(0,0,0,0.25)';
+      ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+      const currentBuffer = bufferRef.current; // local var for speed
+
+      for (let i = 0; i < 256; i++) {
+        const row = i >> 4;
+        const col = i & 15;
+
+        const value = currentBuffer[i] >> 4;
+
+        ctx.fillStyle = fills[value];
+        ctx.fillRect(
+          MARGIN_X + col * PIX_STRIDE_X,
+          MARGIN_Y + row * PIX_STRIDE_Y,
+          PIX_W,
+          PIX_H
+        );
+      }
     };
 
     // kick off the RAF loop
@@ -26,5 +84,5 @@ export const VFBCanvas = () => {
     };
   }, []);
 
-  return <canvas width="360" height="520" ref={canvasRef} />;
+  return <canvas width={WIDTH} height={HEIGHT} ref={canvasRef} />;
 };
