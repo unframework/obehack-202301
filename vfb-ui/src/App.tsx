fc06f691dbbd927aca1dcaafec530b058f2ae7af
Nick Matantsev <nick.matantsev@gmail.com>
2023-02-17 22:05:58 -0500

    Run the loaded Emscripten code

--- vfb-ui/src/App.tsx
+++ vfb-ui/src/App.tsx
@@ -7,13 +7,48 @@ const CODE_SERVER_URL = '/api'; // proxied http://localhost:3000/
 const dummyBuffer = new Uint8Array(256);
 dummyBuffer[2] = 123;
 
+interface VFBRendererModule extends EmscriptenModule {
+  ccall: typeof ccall;
+  cwrap: typeof cwrap;
+}
+
 export const App = () => {
   useEffect(() => {
+    let unmounted = false;
+
     import(/* webpackIgnore: true */ `${CODE_SERVER_URL}/vfb_main.js`).then(
       (vfbModule) => {
-        console.log('imported', vfbModule);
+        // check if already unmounted
+        if (unmounted) {
+          return;
+        }
+
+        console.log('imported module');
+
+        const factory: () => Promise<VFBRendererModule> = vfbModule.default;
+
+        factory().then((m) => {
+          console.log('initialized Emscripten code', m);
+
+          // WASM memory is not expected to grow dynamically
+          const renderBufferPtr = m.ccall('getVirtualBuffer', 'number', [], []);
+          const renderView = m.HEAPU8.subarray(
+            renderBufferPtr,
+            renderBufferPtr + 256
+          );
+
+          console.log('before', renderView);
+          const renderer = m.cwrap('renderVirtualBuffer', null, ['number']);
+
+          renderer(renderBufferPtr);
+          console.log('after', renderView);
+        });
       }
     );
+
+    return () => {
+      unmounted = true;
+    };
   }, []);
 
   return (
