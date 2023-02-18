9088024b2cbeb5ccc3ce506fbda83beee9edd2a5
Nick Matantsev <nick.matantsev@gmail.com>
2023-02-18 00:02:18 -0500

    Better error reporting while rebuilding

--- vfb-wrapper/server.js
+++ vfb-wrapper/server.js
@@ -1,4 +1,4 @@
-const { exec, execSync } = require('child_process');
+const { spawn, execSync } = require('child_process');
 const express = require('express');
 
 const BUILD_DIR = `${__dirname}/build`;
@@ -9,31 +9,42 @@ execSync(`mkdir ${BUILD_DIR}`);
 const app = express();
 app.use(express.static(BUILD_DIR));
 app.post('/build', (req, res) => {
-  // clean first
-  exec(`rm -f "${BUILD_DIR}/*"`, (error) => {
-    if (error) {
-      console.error(error);
+  // build the new file
+  const outJSFile = `vfb${Date.now()}.js`;
+
+  const process = spawn(
+    '/bin/bash',
+    [
+      '-e',
+      '-c',
+      [
+        `rm -f "${BUILD_DIR}/*"`,
+        `emcc vfb_main.cpp effects/renderer.cpp -o build/${outJSFile} -sEXPORT_ES6 -sEXPORTED_RUNTIME_METHODS=ccall,cwrap`,
+      ].join('&&'),
+    ],
+    {
+      stdio: 'pipe',
+    }
+  );
+
+  process.on('exit', (code) => {
+    if (code !== 0) {
+      console.error('unsuccessful exit code', code);
       res.status(500);
       res.send('');
       return;
     }
 
-    // build the new file
-    const outJSFile = `vfb${Date.now()}.js`;
-
-    exec(`emcc vfb_main.cpp effects/renderer.cpp -o build/${outJSFile} -sEXPORT_ES6 -sEXPORTED_RUNTIME_METHODS=ccall,cwrap`, () => {
-      if (error) {
-        console.error(error);
-        res.status(500);
-        res.send('');
-        return;
-      }
-
-      // report the generated file name
-      res.status(200);
-      res.send(`/${outJSFile}`);
-    });
+    // report the generated file name
+    res.status(200);
+    res.send(`/${outJSFile}`);
   });
-})
+
+  process.on('error', (error) => {
+    console.error('spawn error', error);
+    res.status(500);
+    res.send('');
+  });
+});
 
 const server = app.listen(3000);
