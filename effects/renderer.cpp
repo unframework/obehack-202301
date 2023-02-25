032d70dad91802ed1b56701d0b8074b80ee733c2
Nick Matantsev <nick.matantsev@gmail.com>
2023-02-25 02:36:16 -0500

    Fix compilation flags for using FastLED helper functions

--- effects/renderer.cpp
+++ effects/renderer.cpp
@@ -1,3 +1,5 @@
+#include <FastLED.h>
+
 #include "../effects.h"
 
 void render16x16(unsigned char *buffer) {
@@ -6,10 +8,11 @@ void render16x16(unsigned char *buffer) {
     const int col = pos & 15;
     const int row = pos >> 4;
 
-    const unsigned char gradient = (col << 4); // expand to full 8 bits
-    const unsigned char antiGradient = ((15 - col) << 4); // expand to full 8 bits
-    const unsigned char value =
-        (col + row) & 1 ? (row & 1 ? antiGradient : gradient) : 0;
+    const unsigned char value = inoise8(col, row);
+    // const unsigned char gradient = (col << 4); // expand to full 8 bits
+    // const unsigned char antiGradient = ((15 - col) << 4); // expand to full 8 bits
+    // const unsigned char value =
+    //     (col + row) & 1 ? (row & 1 ? antiGradient : gradient) : 0;
 
     buffer[pos] = value;
   }
