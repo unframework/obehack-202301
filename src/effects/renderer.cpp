b7c36767dae01d31b46117182e48a58767e5d74a
Nick Matantsev <nick.matantsev@gmail.com>
2023-04-03 03:31:43 -0400

    Add vertical multi-sampling

--- src/effects/renderer.cpp
+++ src/effects/renderer.cpp
@@ -4,6 +4,8 @@
 
 #include "../effects.h"
 
+#define SUBROWS 31
+
 void render16x16(unsigned char *buffer) {
   const unsigned long tFract = millis() >> 4;
 
@@ -14,25 +16,36 @@ void render16x16(unsigned char *buffer) {
 
     // wispy smoke effect on white bg
     // with slight quadratic warp on the y-coord to compress things near the top
-    // @todo multisampling?
-    const long invrow = (ROWS - row);
-    const long xLookup = col * 7000;
-    const long yLookup =
-        row * 0x3000 - invrow * invrow * 0x200 + tFract * 0x600;
-    const float noiseX = (float)inoise16(xLookup, yLookup, 0) / 0x10000;
+    // multisampling at two positions per row
+    float accum = 0.0f;
 
-    const float dispAmountX = noiseX - 0.5f;
-    const float displacedX = col + dispAmountX * 21.0f * (.2f + invrow * 0.1f);
+    for (int i = 0; i < 2; i++) {
+      const long subrow = (row << 1) + i;
 
-    // @todo feed a vertical displacement (noiseY) into this
-    const float noiseV =
-        fmin(1.0f, row * (0.4f / ROWS) +
-                       (float)inoise16(0x80000, yLookup, 0) / 0x10000);
+      const long invrow = (SUBROWS - subrow);
+      const long xLookup = col * 7000;
+      const long yLookup =
+          subrow * 0x1800 - invrow * invrow * 0x80 + tFract * 0x600;
+      const float noiseX = (float)inoise16(xLookup, yLookup, 0) / 0x10000;
 
-    const unsigned char value =
-        255 * (1.0f - noiseV * fmax(0, fmin(1.0f, fmin(displacedX - 6.0f,
-                                                       9.0f - displacedX) *
-                                                      1.2f)));
+      const float dispAmountX = noiseX - 0.5f;
+      const float displacedX =
+          col + dispAmountX * 21.0f * (.2f + invrow * 0.05f);
+
+      // @todo feed a vertical displacement (noiseY) into this
+      const float noiseV =
+          fmin(1.0f, subrow * (0.5f / SUBROWS) +
+                         (float)inoise16(0x80000, yLookup, 0) / 0x10000);
+
+      accum += fmax(0.0f,
+                    1.0f - noiseV * fmax(0, fmin(1.0f, fmin(displacedX - 6.0f,
+                                                            9.0f - displacedX) *
+                                                           1.2f)));
+    }
+
+    accum /= 2.0f;
+    const unsigned char value = 255 * accum;
+
     // value = displacedX > 6.5f && displacedX < 8.5f ? 255 : 0;
 
     buffer[pos] = value;
