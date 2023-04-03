f28113ed5b83b4378ce96c08a1088ad4cfd4342f
Nick Matantsev <nick.matantsev@gmail.com>
2023-04-03 03:16:29 -0400

    Smoke compression effect near the top

--- src/effects/renderer.cpp
+++ src/effects/renderer.cpp
@@ -13,17 +13,21 @@ void render16x16(unsigned char *buffer) {
     const long row = pos >> 4;
 
     // wispy smoke effect on white bg
-    const float noiseX =
-        (float)inoise16(col * 7000, row * 0x3000 + tFract * 0x800, 0) / 0x10000;
+    // with slight quadratic warp on the y-coord to compress things near the top
+    // @todo multisampling?
+    const long invrow = (ROWS - row);
+    const long xLookup = col * 7000;
+    const long yLookup =
+        row * 0x3000 - invrow * invrow * 0x200 + tFract * 0x600;
+    const float noiseX = (float)inoise16(xLookup, yLookup, 0) / 0x10000;
+
     const float dispAmountX = noiseX - 0.5f;
-    const float displacedX =
-        col + dispAmountX * 21.0f * (.4f + (ROWS - row) * 0.08f);
+    const float displacedX = col + dispAmountX * 21.0f * (.2f + invrow * 0.1f);
 
     // @todo feed a vertical displacement (noiseY) into this
-    const float noiseV = fmin(
-        1.0f, row * (0.4f / ROWS) +
-                  (float)inoise16(0x80000, row * 0x3000 + tFract * 0x800, 0) /
-                      0x10000);
+    const float noiseV =
+        fmin(1.0f, row * (0.4f / ROWS) +
+                       (float)inoise16(0x80000, yLookup, 0) / 0x10000);
 
     const unsigned char value =
         255 * (1.0f - noiseV * fmax(0, fmin(1.0f, fmin(displacedX - 6.0f,
